4a3a813d6d99599da9c41b8ca1822f52a4f95d75
Eduardo Motta de Moraes <edumoraes@live.com>
2025-03-03 20:33:46 -0300

    feat: add script to create first superuser

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,16 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     SECRET_KEY: str = "a89fdb83be9832133abca00a6420cfd5448e194449a272ed2d8a3f938254aecc"
+
+    DATABASE_URL: str = "sqlite:///./local.db"
+    TEST_DATABASE_URL: str = "sqlite:///:memory:"
+
+    FIRST_SUPERUSER_USERNAME: str = "admin"
+    FIRST_SUPERUSER_PASSWORD: str = "admin"
+    FIRST_SUPERUSER_EMAIL: str = "admin@example.com"
+    FIRST_SUPERUSER_FIRST_NAME: str = "First"
+    FIRST_SUPERUSER_LAST_NAME: str = "Superuser"
+
     AUTH_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
@@ -12,8 +22,6 @@ class Settings(BaseSettings):
     STOCKS_ROUTE_STR: str = "stocks"
 
     USERNAME_MAX_LENGTH: int = 30
-
-    TEST_DATABASE_URL: str = "sqlite:///:memory:"
 
 
 settings = Settings()
